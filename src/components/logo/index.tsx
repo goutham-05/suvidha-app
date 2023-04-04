02172ae260ed8c1cde68790b9094d67b77bcfed5
goutham-05 <goutham1494@gmail.com>
2023-04-04 19:25:52 +0100

    feat: inital commit added react-router, semantci-ui-react

---
+++ src/components/logo/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Grid, Icon, Image } from "semantic-ui-react";
+
+function BrandLogo() {
+  return (
+    <Icon.Group size="huge">
+      <Icon size="big" name="circle outline" />
+      <Icon name="user" />
+    </Icon.Group>
+  );
+}
+
+export default BrandLogo;
---
+++ src/features/login/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Grid, Icon, Image } from "semantic-ui-react";
+
+function Login() {
+  return (
+    <Grid container centered>
+      <Grid.Column>
+        <Image src="/images/wireframe/image.png" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src="/images/wireframe/image.png" />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src="/images/wireframe/image.png" />
+      </Grid.Column>
+    </Grid>
+  );
+}
+
+export default Login;
