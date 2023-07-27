e2573884fe06c1053aa79042353abc6720d9028a
Your Name <sait0662@gmail.com>
2023-07-27 12:37:53 +0530

    bk

--- src/Admin/common/SideBar/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./index.css";
-function SideBar() {
-  return <div className="box1">Box 1</div>;
-}
-
-export default SideBar;
